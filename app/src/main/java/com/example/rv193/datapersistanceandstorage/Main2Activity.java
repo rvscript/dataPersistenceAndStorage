cc23f3fe30a3083bdd932b33e20bc54ca5567ff2
Ricardo <rv193@msn.com>
2017-07-15 17:30:57 -0400

    sharedpreferences example

--- app/src/main/java/com/example/rv193/datapersistanceandstorage/Main2Activity.java
+++ app/src/main/java/com/example/rv193/datapersistanceandstorage/Main2Activity.java
@@ -1,29 +1,46 @@
 package com.example.rv193.datapersistanceandstorage;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
-
+    public static final String DEFAULT="N/A";
+    EditText un, passwordet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        un = (EditText)findViewById(R.id.editText3);
+        passwordet=(EditText)findViewById(R.id.editText4);
+    }
+    public void load(View view){
+        SharedPreferences sharedPreferences = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String name = sharedPreferences.getString("name", DEFAULT);
+        String password = sharedPreferences.getString("password", DEFAULT);
+        if(name.equals(DEFAULT)|| password.equals(DEFAULT))
+        {
+            Toast.makeText(this, "No Data was Found", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Data loaded ok", Toast.LENGTH_SHORT).show();
+            un.setText(name);
+            passwordet.setText(password);
+        }
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void previous(View view){
+        Toast.makeText(this, "Previous", Toast.LENGTH_SHORT).show();
+        Intent intent=new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/rv193/datapersistanceandstorage/MainActivity.java
+++ app/src/main/java/com/example/rv193/datapersistanceandstorage/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.rv193.datapersistanceandstorage;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText userName;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userName = (EditText)findViewById(R.id.editText1);
+        password = (EditText)findViewById(R.id.editText2);
+    }
+    public void save(View view){
+        SharedPreferences sharedPreferences= getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("name",userName.getText().toString());
+        editor.putString("password", password.getText().toString());
+        editor.commit();
+        Toast.makeText(this, "Data was saved ok",Toast.LENGTH_LONG).show();
+    }
+    public void next(View view){
+        Toast.makeText(this, "Next", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 }
